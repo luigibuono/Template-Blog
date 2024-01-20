42c568d96f56982359eca88787da091b1a5f401f
Luigi Buono <luigibuono1611@gmail.com>
2024-01-20 16:58:47 +0100

    Update header.component.ts

--- src/app/componenti/header/header.component.ts
+++ src/app/componenti/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  private lastScrollPosition = 0;
+  public isScrolledUp = false;
+  isNavbarCollapsed = true;
 
+  constructor(private el: ElementRef) {}
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+    this.isScrolledUp = currentScrollPosition < this.lastScrollPosition;
+
+    this.lastScrollPosition = currentScrollPosition;
+  }
+
+  getNavbarClasses() {
+    return {
+      'navbar-fixed-top': this.isScrolledUp,
+      'navbar-static-top': !this.isScrolledUp
+    };
+  }
+
+  toggleNavbar() {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
 }
+
