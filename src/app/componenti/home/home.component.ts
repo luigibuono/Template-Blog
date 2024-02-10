76ba36634bdb2769a0d0aa3e99dff8fdfa4e64a1
Luigi Buono <luigibuono1611@gmail.com>
2024-02-10 14:29:30 +0100

    Update home.component.ts

--- src/app/componenti/home/home.component.ts
+++ src/app/componenti/home/home.component.ts
@@ -18,6 +18,18 @@ export class HomeComponent {
     
   ];
 
+  @ViewChild('cardContainer2') cardContainer2!: ElementRef; // Riferimento al container delle card
+  cards2 = [
+    { imageUrl: 'path-to-image1.jpg', alt: 'Image 1', title: 'Titolo 1', description: 'Descrizione breve 1' },
+    { imageUrl: 'path-to-image2.jpg', alt: 'Image 2', title: 'Titolo 2', description: 'Descrizione breve 2' },
+    { imageUrl: 'path-to-image1.jpg', alt: 'Image 1', title: 'Titolo 1', description: 'Descrizione breve 1' },
+    { imageUrl: 'path-to-image2.jpg', alt: 'Image 4', title: 'Titolo 2', description: 'Descrizione breve 2' },
+    { imageUrl: 'path-to-image1.jpg', alt: 'Image 5', title: 'Titolo 1', description: 'Descrizione breve 1' },
+    { imageUrl: 'path-to-image2.jpg', alt: 'Image 6', title: 'Titolo 2', description: 'Descrizione breve 2' },
+    // Aggiungi altre card...
+    
+  ];
+
   scrollCards(direction: 'left' | 'right'): void {
     const container = this.cardContainer.nativeElement;
     const scrollStep = 200; // Regola la quantità di scorrimento
@@ -28,4 +40,16 @@ export class HomeComponent {
       container.scrollLeft += scrollStep; // Scorri a destra
     }
   }
+
+  scrollCards2(direction: 'left' | 'right'): void {
+    const container2 = this.cardContainer2.nativeElement;
+    const scrollStep2 = 200; // Regola la quantità di scorrimento
+
+    if (direction === 'left') {
+      container2.scrollLeft -= scrollStep2; // Scorri a sinistra
+    } else {
+      container2.scrollLeft += scrollStep2; // Scorri a destra
+    }
+  }
+
 }
